9c9646d0c48d8714f7e0f553a632d5e2bd3425cd
mkerchen13 <kimalyajargarokdottir@gmail.com>
2023-02-21 15:00:07 -0800

    Added Data and started creating plot

---
+++ Week_01/Scripts/2023-02-21 - Tidy Tuesday Week 1.R
@@ -0,0 +1,12 @@
+#### Tidy Tuesday Plot: Week 1 ####
+### Created by: Mikayla Kerchen
+### Updated on: 2023-02-21
+########################################
+
+#### Load Libraries ####
+library(tidyverse)
+library(here)
+
+#### Load Data ####
+
+BobRoss_Data <- read.csv(here("Week_01", "Data", "bob_ross.csv"))
