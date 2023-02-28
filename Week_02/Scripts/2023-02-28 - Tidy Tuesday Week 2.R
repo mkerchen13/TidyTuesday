3a5ed48528e57ed7c0a63b55334b95273eda76d3
mkerchen13 <kimalyajargarokdottir@gmail.com>
2023-02-28 15:06:29 -0800

    Fixed a few things

--- Week_02/Scripts/2023-02-28 - Tidy Tuesday Week 2.R
+++ Week_02/Scripts/2023-02-28 - Tidy Tuesday Week 2.R
@@ -17,24 +17,38 @@ View(NumbatsData)
 
 #### Data Analysis and Plotting ####
 
-Australia <- map_data("world", region = "Australia")
+Australia <- map_data("world", region = "Australia")     # Allows me to make a plot using the shape of Australia
 
-NumbatsData_Clean <- na.omit(NumbatsData) %>%
-  group_by(decimalLongitude, decimalLatitude, eventDate) %>%
-  mutate(long = decimalLongitude,
-         lat = decimalLatitude)
-
-merge_data <- full_join(Australia, NumbatsData_Clean)
+WDay_Clean <- NumbatsData %>%              # Removes the NAs from the week day column in the Numbats data frame
+  drop_na(wday)
 
 ggplot() + 
-  geom_polygon(data = Australia, aes(x = long, y = lat, group = group, fill = region), 
+ geom_polygon(data = Australia, aes(x = long, y = lat, group = group, fill = region),      # Only makes the Australia plot 
                color = "black") + 
   facet_wrap(~wday)+
-  geom_point(data = NumbatsData_Clean, aes(x = decimalLongitude, y = decimalLatitude, color = wday), size = 3, na.rm = TRUE)+
-  labs(x = "Longitude",
+  geom_point(data = WDay_Clean, aes(x = decimalLongitude, y = decimalLatitude, color = wday), size = 3)+ # Adds points to the Australia plot
+  labs(x = "Longitude",             # Makes "pretty labels" for the plots
        y = "Latitude",
        fill = "Region",
-       color = "Week Day")
+       color = "Week Day",
+       title = "Numbat Sightings",
+       subtitle = "By Week Day")+
+  theme(axis.title = element_text(size = 14,          # Changes the size, color, face and adjustment of the plot titles.
+                                  color = "black"),
+        axis.title.x = element_text(vjust = -0.5),    # Moves the x-axis away from the tick marks
+        plot.title = element_text(size = 16, 
+                                  color = "black", 
+                                  face = "bold", 
+                                  hjust = 0.5),
+        plot.subtitle = element_text(size = 13,
+                                     color = "black",
+                                     hjust = 0.5,
+                                     face = "italic"),
+        axis.text.x = element_text(size = 10,
+                                   color = "black"),
+        axis.text.y = element_text(size = 10,
+                                   color = "black"))+
+  scale_fill_manual(values = "beige")                  # Changed the color of the fill so it didn't match any of the week day points
   
 
 ggsave(here("Week_02","Output","AustraliaPlot.png"),
