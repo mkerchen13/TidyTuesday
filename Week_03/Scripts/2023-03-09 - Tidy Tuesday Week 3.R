c6c8d24e78d8c2a77a568541a30895644c33c92b
mkerchen13 <kimalyajargarokdottir@gmail.com>
2023-03-09 11:40:42 -0800

    Added comments

--- Week_03/Scripts/2023-03-09 - Tidy Tuesday Week 3.R
+++ Week_03/Scripts/2023-03-09 - Tidy Tuesday Week 3.R
@@ -17,16 +17,16 @@ View(CatvDog_Data)
 
 #### Map Data ####
 
-plot_usmap(data = CatvDog_Data, 
-           values = "percent_cat_owners",
+plot_usmap(data = CatvDog_Data,           # Plots a flat map of the US.
+           values = "percent_cat_owners",  # Uses the data set and plots the percentage data.
            regions = "states",
-           exclude = c("AK", "HI"),
+           exclude = c("AK", "HI"),       # Excluding Alaska and Hawaii from the map since there is no data for them.
            color = "blue")+
-  labs(title = "Cat Owners of the United States",
+  labs(title = "Cat Owners of the United States",         # Adds titles to the plot.
        subtitle = "Percentage of Cat Owners by State")+
-  theme(panel.background = element_blank(),
+  theme(panel.background = element_blank(),             # Makes the map blank inside and put the legend to the right.
         legend.position = "right")+
-  scale_fill_continuous(low = "white", high = "blue", name = "Percentage of Cat Owners")
+  scale_fill_continuous(low = "white", high = "blue", name = "Percentage of Cat Owners")    # Gives a gradient effect in order of how high ot low tyhre percentage is.
 
 ggsave(here("Week_03", "Output", "PercentageCats.png"),
-       width = 7, height = 7)
+       width = 7, height = 7)                                     # Saves the plot and changes the size of the output.
