12d2c74aebda6a30d26bb44d7d63147a4222df5c
mkerchen13 <kimalyajargarokdottir@gmail.com>
2023-04-18 15:02:35 -0700

    Completed Tidy Tuesday for this week

--- Week_07/Scripts/2023-04-18 - Tidy Tuesday Week 7.R
+++ Week_07/Scripts/2023-04-18 - Tidy Tuesday Week 7.R
@@ -23,25 +23,32 @@ view(Egg_Data) # Allows me to view the whole data set
 Egg_Plot <- Egg_Data %>%    # Giving a name to the plot
   mutate(n_eggs = n_eggs/1000000000) %>%   # Dividing the number of eggs by a billion
   ggplot()+                                 
-  geom_density(aes(x = n_eggs,             
-                   y = ..density..), 
+  geom_density(aes(x = n_eggs), 
                fill = "#F05039",          # Color blind friendly color
                color="#e9ecef",            
                alpha=0.8)+                # Makes density plot see-through
-  theme_bw()+
+  theme_bw()+                             # Black and white theme
   labs(x = "Number of Eggs Produced (Per Billion)",  # Adds names for the x and y axis
-       y = "Density")
+       y = "Density")+
+  theme(axis.text = element_text(size = 10,          # Changes the font size and color on the axis labels
+                                 color = "black"),
+        axis.title = element_text(size = 12,
+                                  color = "black"))
 
 Hen_Plot <- Egg_Data %>%            # Giving a name to the second plot
   mutate(n_hens = n_hens/100000000) %>%    # Dividing the number of hens by 100 million
-  ggplot()+
+  ggplot()+                               # Black and white theme
   geom_density(aes(x = n_hens),       
                fill = "#3D65A5",         # Another color blind friendly color
                color = "#e9ecef",
                alpha = 0.8)+            # Makes density plot see-through
   theme_bw()+
   labs(x = "Number of Hens (Per Hundred Million)",  # Adds names for the x and y axis 
-       y = "Density")
+       y = "Density")+
+  theme(axis.text = element_text(size = 10,
+                                 color = "black"),  # Changes the font size and color for the axis labels
+        axis.title = element_text(size = 12,
+                                  color = "black"))
 
 #### Combining the Plots ####
 
@@ -50,5 +57,15 @@ Egg_Plots <- Hen_Plot+Egg_Plot   # Using patchwork to combine the plots side by 
 Egg_Plots +
   plot_annotation(title = "Egg Production in the U.S.",               # Adds title, subtitle, and caption to new combined plots
                   subtitle = "Comparison of Densities Between Total Eggs and Hens",
-                  caption = "Data obtained from the Tidy Tuesday repository")+
-  plot_annotation(tag_levels = 'A')            # Adds 'A' and 'B' labels to the two different plots
+                  caption = "Data obtained from the Tidy Tuesday repository",
+                  theme(plot.title = element_text(size = 16,   # Changes the font size and color of the text on the plot title and subtitle added by patchwork
+                                 color = "black"),
+       plot.subtitle = element_text(size = 14,
+                                    color = "black")))+
+  plot_annotation(tag_levels = 'A') & # Adds 'A' and 'B' labels to the two different plots
+  theme(text = element_text('serif')) # Changes the font of the plot labels and patchwork additions
+
+#### Save the Plot ####
+
+ggsave(here("Week_07", "Output", "Egg_Prod_Plot.png"),    # Saves the plot and sets the height and width of the image
+       width = 10, height = 8)
