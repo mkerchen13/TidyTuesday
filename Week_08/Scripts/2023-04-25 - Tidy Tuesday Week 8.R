cf7221dad3e9286d5b167284cc2472bc83203aa8
Mikayla Kerchen <124097570+mkerchen13@users.noreply.github.com>
2023-04-26 14:55:39 -0700

    Update 2023-04-25 - Tidy Tuesday Week 8.R
    Finished commenting my code.

--- Week_08/Scripts/2023-04-25 - Tidy Tuesday Week 8.R
+++ Week_08/Scripts/2023-04-25 - Tidy Tuesday Week 8.R
@@ -53,18 +53,18 @@ Plot2 <- Game_Data_Top5 %>%
   theme_bw()+
   scale_fill_viridis_d()+ # Color friendly color palette
   facet_wrap(~gamename)+  # Facet wraps the plot by game name 
-  theme(legend.position = "bottom",  # Places the legend on the bottonm to save space on the image
-        legend.title = element_text(size = 12,     # Changes the size a color of the plot title
+  theme(legend.position = "bottom",  # Places the legend on the bottom to save space on the image
+        legend.title = element_text(size = 12,     # Changes the size and color of the axis and legend titles
                                     color = "black"),
         axis.title = element_text(size = 12,
                                   color = "black"))+
-  labs(x = "Year",
+  labs(x = "Year",                                    # Adds pretty names to the axis labels and the legend
        y = "Peak Number of Players (by Million)",
        fill = "Game Name")
 
 #### Patchwork the Plots Together ####
 
-Game_Plots <- Plot1/Plot2
+Game_Plots <- Plot1/Plot2   # Puts the two plots together with Plot 1 being above Plot 2. Gave the new plot a new name
 
 Game_Plots+
   plot_annotation(title = "Statistics for Top 5 Steam Games",               # Adds title, subtitle, and caption to new combined plots
@@ -72,7 +72,7 @@ Game_Plots+
                   caption = "Data obtained from the Tidy Tuesday repository")+
   plot_annotation(tag_levels = 'A') & # Adds 'A' and 'B' labels to the two different plots
   theme(text = element_text('sans')) & # Changes the font of the plot labels and patchwork additions
-  theme(plot.title = element_text(size = 16,   # Changes the font size and color of the text on the plot title and subtitle added by patchwork
+  theme(plot.title = element_text(size = 16,   # Changes the font size, color, and face of the text on the plot title and subtitle added by patchwork
                                 color = "black",
                                 face = 'bold'),
         plot.subtitle = element_text(size = 14,
